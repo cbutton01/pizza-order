1e6740a896fcdb7ceac6a9d555ecea116af168e2
cb <csbutton01@hotmail.com>
2018-04-06 10:14:40 -0700

    calculate price for size and number of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 function Pizza (type, size) { //pizza constructor
-  var price = this.price;
-  
+  this.price = 0;
   this.type = type;
   this.size = size;
   this.toppings = [];
@@ -8,4 +7,21 @@ function Pizza (type, size) { //pizza constructor
 
 Pizza.prototype.addTopping = function (topping) { //push a new topping to the toppings array
   this.toppings.push(topping);
+}
+
+Pizza.prototype.calculatePrice = function() {
+  if (this.size === "Large") {
+    this.price += 10.00;
+  } else if (this.size === "Medium") {
+    this.price += 8.00;
+  } else if (this.size === "Small") {
+    this.price += 5.00;
+  }
+
+  for (var i = 0; i < this.toppings.length; i++) {
+    this.price += .50;
+  }
+
+  return '$' + this.price.toFixed(2);
+
 }
